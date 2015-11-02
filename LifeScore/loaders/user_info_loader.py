e87687df41021a62b5d408c36e142d638482e17c
Gregory Daniels <danielslorenzoz2011@gmail.com>
2015-11-02 13:10:55 -0500

    Code cleanup

--- LifeScore/loaders/user_info_loader.py
+++ LifeScore/loaders/user_info_loader.py
@@ -16,7 +16,6 @@ class user_info_loader(object):
     
     def load_single(self, cursor):
         single_user_info_data = cursor.fetchone()
-        print single_user_info_data
         if single_user_info_data != None:
             return user_info_bean(single_user_info_data[1],single_user_info_data[0])
         return single_user_info_data
